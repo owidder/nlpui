479847f41a5425c3ef9d07de83909b00a030ffd2
Oliver Widder <oliver.widder@gmail.com>
2019-10-29 21:20:04 +0100

    clickable directory

--- src/pages/tfidf-browser/Directory.tsx
+++ src/pages/tfidf-browser/Directory.tsx
@@ -12,26 +12,31 @@ interface DirectoryState {
 }
 
 interface FolderInfo {
-    folder: string;
-    content: string[];
+    folder: string
+    content: string[]
 }
 
 export class Directory extends React.Component<DirectoryProps, DirectoryState> {
 
     readonly state: DirectoryState = {content: [], currentPath: this.props.path}
 
-    private async readDirectory(): Promise<string[]> {
-        const folderInfo: FolderInfo = await fetch(`/api/folder/${this.state.currentPath}`).then(response => response.json())
-        return folderInfo.content
+    private async gotoPath(path: string) {
+        const folderInfo: FolderInfo = await fetch(`/api/folder/${path}`).then(response => response.json())
+        this.setState({content: folderInfo.content, currentPath: path})
     }
 
     async componentDidMount() {
-        this.setState({content: await this.readDirectory()})
+        this.gotoPath(this.props.path)
     }
 
     render() {
         return <div className="directory">
-            {this.state.content.map((entry, i) => <div key={i}>{entry}</div>)}
+            {this.state.content.map(entry => {
+                const newPath = `${this.state.currentPath}/${entry}`
+                return <a key={entry}
+                          href={`#${this.state.currentPath}/${entry}`}
+                          onClick={() => this.gotoPath(newPath)}>{entry}</a>
+            })}
         </div>
     }
 }
