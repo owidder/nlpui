1612a46fd8fdabf8f387a4c3c449d9a03aaab7c9
Oliver Widder <oliver.widder@gmail.com>
2019-10-31 14:06:18 +0100

    queryUtil

--- src/pages/tfidf-browser/index.tsx
+++ src/pages/tfidf-browser/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import {getHashParamValue} from "../../util/queryUtil2";
 import {Directory} from "./Directory";
 
 import "materialize-css/dist/css/materialize.css";
 
+const path = getHashParamValue("path", "");
 
-ReactDOM.render(<Directory path="erpnext"/>, document.getElementById("container"));
+ReactDOM.render(<Directory path={path}/>, document.getElementById("container"));
---
+++ src/util/queryUtil2.ts
@@ -0,0 +1,50 @@
+import {stringify} from "query-string";
+
+const queryString = require('query-string');
+
+const getHashParamMap = () => {
+    const hash = window.location.hash;
+    return queryString.parse(hash);
+}
+
+export const getSearchParamValue = (paramName: string) => {
+    const search = window.location.search;
+    const parsed = queryString.parse(search);
+
+    return parsed[paramName];
+}
+
+export const getHashParamValue = (paramName: string, defValue?: string) => {
+    const hash = window.location.hash;
+    const parsed = queryString.parse(hash);
+
+    return parsed[paramName] || "";
+}
+
+const doCutTrailingSlash = (value: string, cutTrailingSlash: boolean) => {
+    if(cutTrailingSlash && value && value.length > 0 && value.endsWith("/")) {
+        return value.substr(0, value.length-1);
+    }
+
+    return value;
+}
+
+export const getParamValue = (paramName: string, cutTrailingSlash = false) => {
+    const searchParamValue = getSearchParamValue(paramName);
+    if(searchParamValue) {
+        return doCutTrailingSlash(searchParamValue, cutTrailingSlash);
+    }
+
+    return doCutTrailingSlash(getHashParamValue(paramName), cutTrailingSlash);
+}
+
+export const getParamValueWithDefault = (paramName: string, defaultValue: string) => {
+    const param = getParamValue(paramName);
+    return param ? param : defaultValue;
+}
+
+export const setHashValue = (name: string, value: string | number) => {
+    const hashParamMap = getHashParamMap();
+    const newHashParamMap = {...hashParamMap, [name]: value};
+    window.location.hash = queryString.stringify(newHashParamMap);
+}
