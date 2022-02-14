1b3dcba4f2320afda55acd67bf37b2a08ad925fe
Oliver Widder <oliver.widder@gmail.com>
2022-02-14 18:10:34 +0100

    metrics (3)

--- src/pages/cosine-browser/index.tsx
+++ src/pages/cosine-browser/index.tsx
@@ -3,10 +3,11 @@ import * as ReactDOM from "react-dom"
 
 import {getHashParamValue} from "../../util/queryUtil2"
 import {SourceDirectory} from "./SourceDirectory"
+import {DEFAULT_METRIC} from "./metrics";
 
 import "materialize-css/dist/css/materialize.css"
 
 const path = getHashParamValue("path", ".");
-const showAttr = getHashParamValue("showAttr", ".");
+const showAttr = getHashParamValue("showAttr", DEFAULT_METRIC);
 
 ReactDOM.render(<SourceDirectory path={path} showAttr={showAttr} staticFolderCall={false}/>, document.getElementById("container"))
