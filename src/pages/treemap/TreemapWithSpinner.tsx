a408da418e460749698f69b4417149797f7f4e39
Oliver Widder <oliver.widder@gmail.com>
2022-03-12 15:20:04 +0100

    TreemapWithSpinner.tsx

---
+++ src/pages/treemap/TreemapWithSpinner.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import {useEffect, useState} from "react";
+import {showTreemap} from "./treemapGraph";
+import {streamContentWithProgress} from "../stream/streamContentWithProgress";
+import {setHashValue, removeHashName} from "../../util/queryUtil2"
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { css } from "@emotion/react";
+
+const override = css`
+  position: absolute;
+  top: 50vh;
+  left: 50vw;
+`;
+
+import "./tree.scss"
+
+interface TreemapWithProgressProps {
+    zoomto: string
+    width: number
+    height: number
+    currentMetric: string
+}
+
+const newZoomtoCallback = (newZoomto: string) => {
+    if(newZoomto != "." && newZoomto.length > 0) {
+        setHashValue("zoomto", newZoomto.startsWith("./") ? newZoomto.substr(2) : newZoomto)
+    } else {
+        removeHashName("zoomto")
+    }
+}
+
+export const TreemapWithSpinner = ({width, height, zoomto, currentMetric}: TreemapWithProgressProps) => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadingEnded = (tree: any) => {
+            setLoading(false);
+            showTreemap("#treemap", tree, width, height, newZoomtoCallback, zoomto, currentMetric);
+        }
+
+        streamContentWithProgress(`/api/subAgg/folder/`,
+            () => {}, () => {}, () => {}, loadingEnded);
+    }, [])
+
+    return <div id="treemap">
+        {
+            loading ?
+                <ClimbingBoxLoader color="blue" css={override} loading={loading} size={100}/> : <span/>
+        }
+    </div>
+}
--- src/pages/treemap/index.tsx
+++ src/pages/treemap/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 
 import {getHashParamValue} from "../../util/queryUtil2"
-import {TreemapWithProgress} from "./TreemapWithProgress"
+import {TreemapWithSpinner} from "./TreemapWithSpinner"
 
 import "materialize-css/dist/css/materialize.css"
 
@@ -11,4 +11,4 @@ const currentMetric = getHashParamValue("currentMetric", "sum")
 
 document.querySelector(".homelink a").setAttribute("href", "/treemap/treemap.html");
 
-ReactDOM.render(<TreemapWithProgress zoomto={zoomto} width={window.innerWidth} height={window.innerHeight} currentMetric={currentMetric}/>, document.getElementById("container"))
+ReactDOM.render(<TreemapWithSpinner zoomto={zoomto} width={window.innerWidth} height={window.innerHeight} currentMetric={currentMetric}/>, document.getElementById("container"))
