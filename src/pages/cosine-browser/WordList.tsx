9d18c87b214e4c6121cc0ba637127a8f3917f73f
Oliver Widder <oliver.widder@gmail.com>
2022-05-05 19:02:54 +0200

    links in WordList.tsx

--- src/pages/cosine-browser/WordList.tsx
+++ src/pages/cosine-browser/WordList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import {WordAndMetrics} from "./metrics";
+import {currentLocationWithNewHashValues} from "../../util/queryUtil2";
 
 interface WordListProps {
     currentMetric: string
@@ -16,7 +17,7 @@ export const WordList = ({currentMetric, wordsAndMetrics}: WordListProps) => {
             sortedWordsAndMetrics.map((wordAndMetrics, index) => {
                 return <div className="listrow" key={index}>
                     <div className="cell index">{index}</div>
-                    <div className="cell string">{wordAndMetrics.word}</div>
+                    <div className="cell string"><a onClick={() => document.dispatchEvent(new CustomEvent('reload'))} href={currentLocationWithNewHashValues({feature: wordAndMetrics.word})}>{wordAndMetrics.word}</a></div>
                     <div className="cell">{wordAndMetrics[currentMetric].toFixed(2)}</div>
                 </div>
             }) :
