5aa7099b7ca59fe6b6b7b068a395ae553284a4f4
Oliver Widder <oliver.widder@gmail.com>
2022-05-14 16:46:19 +0200

    logging

--- src/pages/srcFromPath.ts
+++ src/pages/srcFromPath.ts
@@ -1,6 +1,7 @@
 const _path = require("path");
 
 export const srcPathFromPath = (path: string, srcPathMap: {[name: string]: string}): string => {
+    console.log(`path = ${path}`)
     const pathPrefixes = Object.keys(srcPathMap as any | {});
 
     if(pathPrefixes.length < 1) return "";
@@ -8,7 +9,9 @@ export const srcPathFromPath = (path: string, srcPathMap: {[name: string]: strin
     for(let p = 0; p < pathPrefixes.length; p++) {
         const pathPrefix = pathPrefixes[p];
         if(path.startsWith(pathPrefix) && srcPathMap[pathPrefix].length > 0) {
-            return _path.join(srcPathMap[pathPrefix], path.substring(pathPrefix.length))
+            const srcPath = _path.join(srcPathMap[pathPrefix], path.substring(pathPrefix.length));
+            console.log(`srcPath = ${srcPath}`);
+            return srcPath
         }
     }
 
