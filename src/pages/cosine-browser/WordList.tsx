28bcc24b14b67359f5eff3490be652405784340a
Oliver Widder <oliver.widder@gmail.com>
2022-06-05 16:48:13 +0200

    title in WordList.tsx

--- src/pages/cosine-browser/WordList.tsx
+++ src/pages/cosine-browser/WordList.tsx
@@ -13,10 +13,15 @@ export const WordList = ({currentMetric, wordsAndMetrics}: WordListProps) => {
     const sortedWordsAndMetrics = wordsAndMetrics.sort((a, b) => b[currentMetric] - a[currentMetric]);
 
     return <div className="directory list">
+        <div className="listrow title">
+            <div className="cell index">No.</div>
+            <div className="cell string">stem and long words</div>
+            <div className="cell">{currentMetric}</div>
+        </div>
         {sortedWordsAndMetrics.length > 0 ?
             sortedWordsAndMetrics.map((wordAndMetrics, index) => {
                 return <div className="listrow" key={index}>
-                    <div className="cell index">{index}</div>
+                    <div className="cell index">{index+1}</div>
                     <div className="cell string">
                         <span>{wordAndMetrics.stem}: </span>{wordAndMetrics.words.map((word, index2) => <span key={index2} ><a onClick={() => document.dispatchEvent(new CustomEvent('reload'))} href={currentLocationWithNewHashValues({feature: word, currentMetric})}>{word}</a>&nbsp;</span>)}
                     </div>
